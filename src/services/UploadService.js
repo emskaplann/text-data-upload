2af25784ce28839607a290618e6ce59ba6ea1dde
Emirhan Kaplan <emskaplann@gmail.com>
2020-10-21 20:42:27 -0400

    update the repo

---
+++ src/services/UploadService.js
@@ -0,0 +1,11 @@
+export default class UploadService {
+    constructor(component) {
+        this.testURL = ""
+        this.prodURL = ""
+        this.component = component
+    }
+
+    uploadFileToParse(requestBody) {
+        //fetch
+    }
+}
