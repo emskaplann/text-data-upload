d5f0d04d3d98d3a9becb8079ea20a47aa1256235
Emirhan Kaplan <emskaplann@gmail.com>
2020-10-24 22:50:16 -0400

    Updates

---
+++ precache-manifest.f2edb5696bd79dc5aad17b9beb35f47e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4c965d98c6a85e90e47c870d975f6f",
+    "url": "/text-data-upload/index.html"
+  },
+  {
+    "revision": "96096835023f9d14f7d3",
+    "url": "/text-data-upload/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b3f2a9327772fe4685e4",
+    "url": "/text-data-upload/static/css/main.9870ccf5.chunk.css"
+  },
+  {
+    "revision": "96096835023f9d14f7d3",
+    "url": "/text-data-upload/static/js/2.88c9d3cc.chunk.js"
+  },
+  {
+    "revision": "7a8d23535d0e652dc6bb150441ce0cac",
+    "url": "/text-data-upload/static/js/2.88c9d3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f2a9327772fe4685e4",
+    "url": "/text-data-upload/static/js/main.4c7a0e67.chunk.js"
+  },
+  {
+    "revision": "808a2103f74be8598dc3",
+    "url": "/text-data-upload/static/js/runtime-main.5b626fc4.js"
+  }
+]);
